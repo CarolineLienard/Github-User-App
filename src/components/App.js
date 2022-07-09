aeaf232b04a63204278810e438c14a44464f26bd
Caroline Lienard <carolinemrd@outlook.fr>
2022-07-09 21:28:57 +0200

    ✨ Adding light and dark theme feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
 import '../sass/main.scss';
-
 import Header from './Header';
 import Search from './Search';
 import User from './User';
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  function toggleTheme () {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  }
+
+  useEffect (() => {
+    const bodyColor = document.body
+    theme === "light" ? bodyColor.id = "light" : bodyColor.id = "dark"
+  },[theme])
+
   return (
-    <main className="flex column">
-      <Header />
-      <Search />
-      <User />
-    </main>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <main className="flex column" id={theme}>
+        <Header themeSwitch={toggleTheme} buttonLabel={theme} />
+        <Search />
+        <User />
+      </main>
+    </ThemeContext.Provider>
   )  
 }
 
