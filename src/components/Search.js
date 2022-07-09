243622c86396a552a405a366b8462964cdf6d935
Caroline Lienard <carolinemrd@outlook.fr>
2022-07-09 15:56:19 +0200

    💄 Update UI

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,18 @@ function Search () {
     return (
         <form className="flex center" action="post" role="search">
             <div>
-            <img src={iconSearch} alt="" />
+                <img src={iconSearch} alt="" />
             </div>
+           
             <input type="search" autoComplete="off" placeholder="Search GitHub username..." name="value" id="initial-input" />
             <input type="search" autoComplete="off" placeholder="Search..." name="value" id="responsive-input" />
 
+            
+            <p className="errorMessage flex end">No result</p>
+            
+
             <div className="button flex end">
-            <button type="submit">Search</button>
+                <button id="search" type="submit">Search</button>
             </div>
       </form>
     )
